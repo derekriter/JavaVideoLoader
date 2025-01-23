41d21e32cf5b45392027d514efa499e3d4fd0370
Derek Riter <derekriter08@gmail.com>
2025-01-23 16:32:52 -0500

    Added video debug info

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,46 @@ class Main {
     public static String videoName = "BadApple.mp4";
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello, world!");
         FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(Main.class.getResourceAsStream(videoName));
         grabber.start();
 
-        System.out.printf("%s x %s\n", grabber.getImageWidth(), grabber.getImageHeight());
+        displayVideoInfo(grabber);
 
         grabber.close();
+    }
+
+    private static void displayVideoInfo(FFmpegFrameGrabber grabber) {
+        long lengthInSeconds = Math.round(grabber.getLengthInTime() / 1e6);
+        long hours = lengthInSeconds / 3600;
+        long minutes = (lengthInSeconds / 60) % 60;
+        long seconds = lengthInSeconds % 60;
+        System.out.printf("Length: %dh:%dm:%ds\n\n", hours, minutes, seconds);
+
+        boolean hasVideo = grabber.hasVideo();
+        System.out.printf("Video:\n\tHas video: %b\n", hasVideo);
+        if(hasVideo) {
+            int resX = grabber.getImageWidth();
+            int resY = grabber.getImageHeight();
+            double aspectRatio = resX / (double) resY;
+            double fps = grabber.getVideoFrameRate();
+            int bitrate = grabber.getVideoBitrate();
+            String codec = grabber.getVideoCodecName();
+            int lengthInFrames = grabber.getLengthInVideoFrames();
+
+            System.out.printf("\tResolution: %dx%d\n\tAspect ratio: %f\n\tFrame rate: %f\n\tBitrate: %d\n\tCodec: %s\n\tLength (in frames): %d\n", resX, resY, aspectRatio, fps, bitrate, codec, lengthInFrames);
+        }
+
+        boolean hasAudio = grabber.hasAudio();
+        System.out.printf("Audio:\n\tHas audio: %b\n", hasAudio);
+        if(hasAudio) {
+            int channels = grabber.getAudioChannels();
+            double fps = grabber.getAudioFrameRate();
+            int bitrate = grabber.getAudioBitrate();
+            String codec = grabber.getAudioCodecName();
+            int sampleRate = grabber.getSampleRate();
+            int lengthInFrames = grabber.getLengthInAudioFrames();
+
+            System.out.printf("\tChannels: %d\n\tFrame rate: %f\n\tBitrate: %d\n\tCodec: %s\n\tSample rate: %d\n\tLength (in frames): %d\n", channels, fps, bitrate, codec, sampleRate, lengthInFrames);
+        }
     }
 }
